2ff8657f488d64e24e1cea77e57289c9d59f2ba6
Igor <fullstack@naladka.su>
2023-04-20 22:11:38 +0300

    edit 18.py

--- 18.py
+++ 18.py
@@ -2,3 +2,27 @@
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих  строках записаны N целых чисел Ai.
 # Последняя строка содержит число X
+
+from random import randint
+N = int(input("\nВведите количество элементов в массиве: "))
+X = int(input("\nВведите число ""X"": "))
+lists = []
+NumberMax = 0
+NumberMin = 0
+for el in range(0, N):
+    i = randint(0, N)
+    lists.append(i)
+for i in range(0,len(lists)):
+    if lists[i] == X+1:
+        NumberMax = lists[i]
+    elif lists[i] == X-1:
+        NumberMin = lists[i]
+print(f"\n{lists}")
+if NumberMax == 0 and NumberMin == 0:
+    print("Нет ближайших номеров.")
+elif NumberMin == 0:
+    print(f"\nБлижайшая цифра в списке к Х = {NumberMax}\n")
+elif NumberMax == 0:
+    print(f"\nБлижайшая цифра в списке к Х = {NumberMin}\n")
+else:
+    print(f"\nБлижайшая к X\nминимальная цифра = {NumberMin}\nмаксимальная цифра = {NumberMax}")
